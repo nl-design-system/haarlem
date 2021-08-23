ccef25444b0fb2215e9e08d6f80beca7442b2cdb
Robbert Broersma <mail@robbertbroersma.nl>
2021-08-23 21:30:15 +0200

    feat: Haarlem theme for Storybook

--- packages/storybook/config/theme.js
+++ packages/storybook/config/theme.js
@@ -4,5 +4,27 @@ import { create } from '@storybook/theming/create';
 // https://storybook.js.org/docs/react/configure/theming
 export default create({
   base: 'light',
-  brandTitle: 'NL Design System: Example',
+  colorPrimary: 'rgb(42, 99, 168)',
+  colorSecondary: 'rgb(200, 58, 63)',
+
+  // UI
+  appBg: 'rgb(235, 236, 236)',
+  appContentBg: 'rgb(255, 255, 255)',
+  appBorderColor: 'rgb(69, 71, 71)',
+  appBorderRadius: 0,
+
+  textColor: 'rgb(59, 62, 62)',
+
+  barTextColor: 'rgb(0, 0, 0)',
+  barSelectedColor: 'rgb(42, 99, 168)',
+  barBg: 'rgb(208, 208, 208)',
+
+  // Typography
+  fontBase: 'Arial, Verdana, sans-serif',
+  fontCode: 'monospace',
+  fontSize: '100%',
+
+  brandTitle: 'Gemeente Haarlem',
+  brandUrl: 'https://www.haarlem.nl',
+  brandImage: 'foot_logo_haarlem_2018.png',
 });
