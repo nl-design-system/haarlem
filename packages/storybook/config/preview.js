729fe6f2fc31cddc78e762a1187a4de69db67b2b
Robbert Broersma <mail@robbertbroersma.nl>
2021-08-23 22:03:43 +0200

    feat: add components from NL Design System community

--- packages/storybook/config/preview.js
+++ packages/storybook/config/preview.js
@@ -1,9 +1,17 @@
 import '@gemeente-haarlem/design-tokens/dist/index.css';
 import '@gemeente-haarlem/design-tokens/src/custom.scss';
 
+import { defineCustomElements as defineUtrechtComponents } from '@utrecht/web-component-library-stencil';
 import { defineCustomElements } from '@gemeente-haarlem/web-components-stencil';
 
 defineCustomElements();
+defineUtrechtComponents();
+
+export const decorators = [
+  // Enable `utrecht-document` component as backdrop
+  // Enable `haarlem-theme` to configure the design tokens
+  // (story) => `<div class="utrecht-document haarlem-theme">${story()}</div>`,
+];
 
 const statuses = {
   BETA: '#9F5E0F',
@@ -18,11 +26,36 @@ const previewTabs = {
   canvas: { title: 'Design Tokens' },
 };
 
+const sidebarOrder = {
+  options: {
+    panelPosition: 'bottom',
+    storySort: {
+      order: [
+        'Haarlem',
+        ['README'],
+        'NL Design System',
+        ['README'],
+        'CSS Component',
+        ['README'],
+        'HTML Component',
+        ['README'],
+        'Web Component',
+        ['README'],
+        'Angular Component',
+        ['README'],
+        'React Component',
+        ['README'],
+        'Vue.js Component',
+        ['README'],
+        'Form.io Component',
+        ['README'],
+      ],
+    },
+  },
+};
 export const parameters = {
   controls: { expanded: false },
   previewTabs,
   statuses,
-  options: {
-    panelPosition: 'right',
-  },
+  ...sidebarOrder,
 };
